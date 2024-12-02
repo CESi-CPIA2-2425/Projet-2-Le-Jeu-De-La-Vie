c4af0ef3b0351e4a576a1b524366c1572d85e7b0
Rayane le Goat <rayane.ould.herault@gmail.com>
2024-12-02 14:33:38 +0100

    Update Fichier.h

--- github_repo/Projet2_LJDLV/Fichier.h
+++ github_repo/Projet2_LJDLV/Fichier.h
@@ -1,9 +1,22 @@
+//
+// Created by Admin on 29/11/2024.
+//
+
 #ifndef FICHIER_H
 #define FICHIER_H
+#include <string>
+#include <fstream>
+#include <vector>
 
-
+using namespace std;
 
 class Fichier {
+    public:
+        string chemin_fichier;
+        Fichier() {} // constructeur
+
+        string lecture(); // methode pour lire le contenue du fichier
+        vector<string> vecteur(string txt);  // Methode pour installer les info dans des vecteur
 
 };
 
