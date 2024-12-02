724bae40740c1c733dedcae36607bed21e1a30e6
Thaïs <125659463+thedarknessqueen@users.noreply.github.com>
2024-12-02 11:23:41 +0100

    Update Ligne.cpp

--- github_repo/Projet2_LJDLV/Ligne.cpp
+++ github_repo/Projet2_LJDLV/Ligne.cpp
@@ -1,1 +1,24 @@
 #include "Ligne.h"
+
+void Ligne::change_etat(){
+    x=0;
+    y=0;
+    int i = 0;
+    while (i<=l*h) {
+        int environ = vec[y-1%h][x-1%h] + vec[y-1%h][x%h] + vec[y-1%h][x+1%h] + vec[y%h][x-1%h] + vec[y%h][x+1%h] + vec[y+1%h][x-1%h] + vec[y+1%h][x%h] + vec[y+1%h][x+1%h];
+        if(vec[y][x]=0) {
+            environ >=3;
+            vec[y][x] = 1;
+        }
+        if(vec[y][x]=1) {
+            environ <=2;
+            vec[y][x]=0;
+        }
+        i=i+1;
+        x=x+1;
+        if(x>l-1) {
+            x=0;
+            y=y+1;
+        }
+    }
+}
