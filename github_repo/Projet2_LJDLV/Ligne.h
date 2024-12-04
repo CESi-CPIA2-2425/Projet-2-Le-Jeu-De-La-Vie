5fa3afff87a59e754f2356a5b3af2ee1fde72a77
Rayane le Goat <rayane.ould.herault@gmail.com>
2024-12-04 02:42:44 +0100

    Update Ligne.h

--- github_repo/Projet2_LJDLV/Ligne.h
+++ github_repo/Projet2_LJDLV/Ligne.h
@@ -1,9 +1,25 @@
+//
+// Created by Admin on 29/11/2024.
+//
+
 #ifndef LIGNE_H
 #define LIGNE_H
+#include <vector>
+#include <string>
 
-#include "Grille.h"
+class Ligne {
 
-class Ligne : public Grille {
+    private:
+        std::vector<std::string>grille;
+        int cologne ;
+        int ligne;
+    public:
+
+    Ligne(std::vector<std::string>grille);// consturcteur temp
+    int CompteCell(int x,int y,std::vector<std::string> grille);
+    void Generation();
+    void AfficheGrille();
+
 
 };
 
