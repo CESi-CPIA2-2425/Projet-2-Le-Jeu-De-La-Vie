1a7f0f198d71dc104a042d4654160b9f46e28857
Rayane le Goat <rayane.ould.herault@gmail.com>
2024-12-04 02:42:07 +0100

    Update Ligne.cpp

--- github_repo/Projet2_LJDLV/Ligne.cpp
+++ github_repo/Projet2_LJDLV/Ligne.cpp
@@ -1,24 +1,57 @@
-#include "Ligne.h"
+//
+// Created by Admin on 29/11/2024.
+//
 
-void Ligne::change_etat(){
-    x=0;
-    y=0;
-    int i = 0;
-    while (i<=l*h) {
-        int environ = vec[y-1%h][x-1%h] + vec[y-1%h][x%h] + vec[y-1%h][x+1%h] + vec[y%h][x-1%h] + vec[y%h][x+1%h] + vec[y+1%h][x-1%h] + vec[y+1%h][x%h] + vec[y+1%h][x+1%h];
-        if(vec[y][x]=0) {
-            environ >=3;
-            vec[y][x] = 1;
-        }
-        if(vec[y][x]=1) {
-            environ <=2;
-            vec[y][x]=0;
-        }
-        i=i+1;
-        x=x+1;
-        if(x>l-1) {
-            x=0;
-            y=y+1;
+#include "Ligne.h"
+#include <iostream>
+
+
+Ligne::Ligne( std::vector<std::string> grille) : grille(grille), ligne(grille.size()), cologne(grille[0].size()) {} //constructeur
+
+int Ligne::CompteCell(int x,int y,std::vector<std::string> grille) {
+    int compteur = 0;
+
+
+    for(int i=-1; i<=1; i++) {
+        for(int j=-1; j<=1; j++) {
+            if(i == 0 && j == 0) continue;
+            int nx = x+i;
+            int ny = y+j;
+            if (nx >= 0 && nx<ligne && ny>= 0 && ny < cologne) {
+                compteur += grille[nx][ny] =='1';
+            }
         }
     }
+    return compteur;
 }
+
+// Affiche la grille actuelle
+void Ligne::AfficheGrille() {
+    for (const auto& l : grille) {
+        std::cout << l << '\n';
+    }
+    std::cout << '\n';
+}
+
+void Ligne::Generation() {
+    std::vector<std::string> nvlle = grille;
+
+    for (int i = 0; i < ligne; ++i) {
+        for (int j = 0; j < cologne; ++j) {
+            int voisin = CompteCell(i, j,grille);
+            if (grille[i][j] == '1') {
+                nvlle[i][j] =(voisin == 2 || voisin ==  3) ? '1' : '0';
+            }else {
+                nvlle[i][j] = (voisin ==3) ? '1' : '0';
+            }
+        }
+    }
+    grille = nvlle;
+
+}
+
+
+
+
+
+
